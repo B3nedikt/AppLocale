a4c0bb39462065cb8fa6c9ad324b65142293f0c6
B3nedikt <mail@b3nedikt.dev>
2021-05-01 19:33:16 +0200

    - Added Release Github Action - Added new Github Actions Badge to README.md

--- docs/module-search-index.js
+++
@@ -1,1 +0,0 @@
-var moduleSearchIndex = [{"l":"applocale","url":"index.html"}]
--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-var packageSearchIndex = [{"l":"dev.b3nedikt.app_locale","url":"dev.b3nedikt.app_locale/package-summary.html"}, {"l":"All packages","url":"index.html"}]
