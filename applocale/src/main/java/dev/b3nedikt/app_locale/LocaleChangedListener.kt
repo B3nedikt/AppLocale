fb9328d1e6f80dcf901aa8ddc415821acb354a8d
B3nedikt <mail@b3nedikt.dev>
2022-02-23 21:36:00 +0100

    Made LocaleChangedListener a functional inteface

--- applocale/src/main/java/dev/b3nedikt/app_locale/LocaleChangedListener.kt
+++ applocale/src/main/java/dev/b3nedikt/app_locale/LocaleChangedListener.kt
@@ -3,7 +3,7 @@ package dev.b3nedikt.app_locale
 /**
  * Listener for changes to the [AppLocale.currentLocale]
  */
-interface LocaleChangedListener {
+fun interface LocaleChangedListener {
 
     /**
      * Called when the [AppLocale.currentLocale] has changed
