596141f8b2331a65e3779cbe2e1e0961b1959a94
B3nedikt <mail@b3nedikt.dev>
2022-02-24 10:15:35 +0100

    Updated the unit tests to use the latest dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,15 +2,15 @@ object Dependencies {
 
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:1.6.0"
 
-    const val appCompat = "androidx.appcompat:appcompat:1.2.0"
-    const val annotation = "androidx.annotation:annotation:1.0.0"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
+    const val appCompat = "androidx.appcompat:appcompat:1.4.1"
+    const val annotation = "androidx.annotation:annotation:1.3.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.3"
     const val viewPump = "dev.b3nedikt.viewpump:viewpump:4.0.10"
     const val reword = "dev.b3nedikt.reword:reword:4.0.1"
 
     const val junit = "junit:junit:4.12"
     const val kluent = "org.amshove.kluent:kluent-android:1.59"
-    const val robolectric = "org.robolectric:robolectric:4.5.1"
-    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
+    const val robolectric = "org.robolectric:robolectric:4.7.3"
+    const val mockitoKotlin = "org.mockito.kotlin:mockito-kotlin:4.0.0"
     const val testCore = "androidx.test:core:1.4.0"
 }
